2d68510a953d97bff9aa01c255042ce237a3d5d0
Jeff Haynie <jhaynie@appcelerator.com>
2015-04-19 09:32:13 -0700

    API-742 remove use of fingerprint for now

--- test/security.test.js
+++ test/security.test.js
@@ -2,7 +2,7 @@ var assert = require('assert'),
 	ArrowDB = require('../index'),
 	fs = require('fs'),
 	path = require('path'),
-	request = require('appc-request-ssl');
+	request = require('request');
 
 describe('Security Test', function() {
 	this.timeout(10000);
@@ -11,7 +11,7 @@ describe('Security Test', function() {
 	var DEFAULT_API_ENTRY_POINT_FINGERPRINT = fs.readFileSync(path.join(__dirname, '..', 'node_modules', 'appc-request-ssl', 'fingerprints', ArrowDB.DEFAULT_API_ENTRY_POINT.replace(/^https?\:\/\//, ''))).toString();
 	var DEFAULT_API_TEST_ENTRY_POINT_FINGERPRINT = fs.readFileSync(path.join(__dirname, '..', 'node_modules', 'appc-request-ssl', 'fingerprints', ArrowDB.DEFAULT_API_TEST_ENTRY_POINT.replace(/^https?\:\/\//, ''))).toString();
 
-	it('should validate DEFAULT_API_ENTRY_POINT',function(done){
+	it.skip('should validate DEFAULT_API_ENTRY_POINT',function(done){
 		if (process.env.TRAVIS) {
 			assert.equal(process.env.FINGERPRINT_DEFAULT.replace(fingerprintRegex,'$1'), DEFAULT_API_ENTRY_POINT_FINGERPRINT);
 			done();
@@ -24,7 +24,7 @@ describe('Security Test', function() {
 		}
 	});
 
-	it('should validate DEFAULT_API_TEST_ENTRY_POINT',function(done){
+	it.skip('should validate DEFAULT_API_TEST_ENTRY_POINT',function(done){
 		if (process.env.TRAVIS) {
 			assert.equal(process.env.FINGERPRINT_TEST.replace(fingerprintRegex,'$1'), DEFAULT_API_TEST_ENTRY_POINT_FINGERPRINT);
 			done();
